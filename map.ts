a32793ea18393b60d57e846c837b4781f0efdb1e
amschel <102370012+amschel99@users.noreply.github.com>
2024-01-09 21:23:32 +0300

    Update map.ts

--- map.ts
+++ map.ts
@@ -5,3 +5,8 @@ function map<Input, Output>(arr:Input[], fn:(param:Input)=>Output):Output[]{
     
   })
 }
+function by2(val:number){
+return val*2;
+}
+const nums=[0,1,2,3,4,5];
+console.log(map(nums, by2));
